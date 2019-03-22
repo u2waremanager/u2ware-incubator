1f5234f9bf0b21ffeaecb1652d99115da4b83908
u2waremanager@gmail.com <u2waremanager@gmail.com>
2019-03-22 15:39:49 +0900

    .

---
+++ io.github.u2ware.frontend.vue/src/node.5/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import './plugins/vuetify'
+import App from './App.vue'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
