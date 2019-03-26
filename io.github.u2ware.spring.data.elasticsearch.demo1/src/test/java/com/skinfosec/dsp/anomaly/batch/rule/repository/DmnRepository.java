7bc1b329647a054dc4390b783f55db8d4b80b8d6
u2waremanager@gmail.com <u2waremanager@gmail.com>
2019-03-26 13:56:11 +0900

    -

--- io.github.u2ware.spring.data.elasticsearch.demo1/src/test/java/com/skinfosec/dsp/anomaly/batch/rule/repository/DmnRepository.java
+++
@@ -1,9 +0,0 @@
-package com.skinfosec.dsp.anomaly.batch.rule.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.skinfosec.dsp.anomaly.batch.rule.Dmn;
-
-public interface DmnRepository extends PagingAndSortingRepository<Dmn, Long>{
-
-}
--- io.github.u2ware.spring.data.elasticsearch.demo1/src/test/java/com/skinfosec/dsp/anomaly/batch/rule/repository/TenantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.skinfosec.dsp.anomaly.batch.rule.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.skinfosec.dsp.anomaly.batch.rule.Tenant;
-
-public interface TenantRepository extends PagingAndSortingRepository<Tenant, Long>{
-
-}
