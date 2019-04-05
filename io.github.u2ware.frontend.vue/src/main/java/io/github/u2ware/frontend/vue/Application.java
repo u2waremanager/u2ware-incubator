872e0dba51e514ef056329ce79156a15ae3e3cd4
u2waremanager@gmail.com <u2waremanager@gmail.com>
2019-04-05 14:39:55 +0900

    -

--- io.github.u2ware.frontend.vue/src/main/java/io/github/u2ware/frontend/vue/Application.java
+++ io.github.u2ware.frontend.vue/src/main/java/io/github/u2ware/frontend/vue/Application.java
@@ -2,19 +2,11 @@ package io.github.u2ware.frontend.vue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@Controller
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@RequestMapping(value = "{path:[^\\\\.]*}")
-	public String html5Forwarding() {
-		return "forward:/index.html";
 	}
 }
---
+++ io.github.u2ware.frontend.vue/src/main/java/io/github/u2ware/frontend/vue/ForwardController.java
@@ -0,0 +1,14 @@
+package io.github.u2ware.frontend.vue;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ForwardController {
+
+	@RequestMapping(value = "{path:[^\\\\.]*}")
+	public String html5Forwarding() {
+		return "forward:/index.html";
+	}
+	
+}
