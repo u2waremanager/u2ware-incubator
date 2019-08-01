4d0f34392e0e2ba90537643c0d0304bec05de162
u2waremanager@gmail.com <u2waremanager@gmail.com>
2019-08-01 12:30:02 +0900

    -

--- io.github.u2ware.frontend.vue/src/apps/demo/test-axios/App.vue
+++ io.github.u2ware.frontend.vue/src/apps/demo/test-axios/App.vue
@@ -3,6 +3,8 @@
 </template>
 
 <script>
+import qs from 'qs';
+
 export default {
 
     methods: {        
@@ -10,23 +12,45 @@ export default {
             this.$log.debug('aaaa');
             this.$log.debug(this.$axios);
 
-            this.$axios
-                .get('http://google.com')
-                .then((result) => {
-                    this.$log.debug(result);
-                })
-                .catch((error) => {
-                    this.$log.debug(error);
-                })
+            // this.$axios
+            //     .get('http://google.com')
+            //     .then((result) => {
+            //         this.$log.debug(result);
+            //     })
+            //     .catch((error) => {
+            //         this.$log.debug(error);
+            //     })
 
-            this.$axios
-                .get('http://sssss/persons2')
-                .then((result) => {
-                    this.$log.debug(result);
-                })
-                .catch((error) => {
-                    this.$log.debug(error);
-                })
+            // this.$axios
+            //     .get('http://sssss/persons2')
+            //     .then((result) => {
+            //         this.$log.debug(result);
+            //     })
+            //     .catch((error) => {
+            //         this.$log.debug(error);
+            //     })
+
+
+            var p = {
+                name : 'a',
+                age : ["1","2"]
+            };
+
+            this.$axios({
+
+                method : 'post',
+                url : 'http://localhost:8080/apis/foos/!q',
+                params : p,
+                paramsSerializer: (params) => {
+                    return qs.stringify(params, {arrayFormat: 'repeat'});
+                },
+
+            }).then((result) => {
+                this.$log.debug(result);
+            })
+            .catch((error) => {
+                this.$log.debug(error);
+            })
 
         }
     }
