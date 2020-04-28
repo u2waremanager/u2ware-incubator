4a78e1b3e040ce09d9a8d6e646a99212abd0574e
u2waremanager@gmail.com <u2waremanager@gmail.com>
2020-04-28 09:43:03 +0900

    -

--- io.github.u2ware.frontend.vue.template/boilerplate/src/routes/demo-kakao-map.js
+++
@@ -1,7 +0,0 @@
-export default [
-    {
-        path: '/demo-kakao-map',
-        name: 'demo-kakao-map',
-        component: () => import(/* webpackChunkName: "demo-kakao-map" */ '../apps/demo-kakao-map/App.vue')
-    },
-]
--- io.github.u2ware.frontend.vue.template/boilerplate/vue.config.js
+++ io.github.u2ware.frontend.vue.template/boilerplate/vue.config.js
@@ -1,3 +1,8 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
@@ -10,5 +15,24 @@ module.exports = {
       localeDir: 'locales',
       enableInSFC: false
     }
-  }
+  },
+  configureWebpack: {
+    plugins: [
+        // Copy Cesium Assets, Widgets, and Workers to a static directory
+        new CopyWebpackPlugin([{ from: 'node_modules/cesium/Build/Cesium/Workers', to: 'Workers' }]),
+        new CopyWebpackPlugin([{ from: 'node_modules/cesium/Build/Cesium/ThirdParty', to: 'ThirdParty' }]),
+        new CopyWebpackPlugin([{ from: 'node_modules/cesium/Build/Cesium/Assets', to: 'Assets' }]),
+        new CopyWebpackPlugin([{ from: 'node_modules/cesium/Build/Cesium/Widgets', to: 'Widgets' }]),
+        new webpack.DefinePlugin({
+          // Define relative base path in cesium for loading assets
+          CESIUM_BASE_URL: JSON.stringify('../../')
+        }),
+    ],
+    module: {
+      // Removes these errors: "Critical dependency: require function is used in a way in which dependencies cannot be statically extracted"
+      // https://github.com/AnalyticalGraphicsInc/cesium-webpack-example/issues/6
+      unknownContextCritical: false,
+      unknownContextRegExp: /\/cesium\/cesium\/Source\/Core\/buildModuleUrl\.js/,
+    }
+  }  
 }
