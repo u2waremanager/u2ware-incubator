454c35525d54ce80991d449c3e898f7bad491e14
u2waremanager@gmail.com <u2waremanager@gmail.com>
2020-05-14 09:17:22 +0900

    -

---
+++ io.github.u2ware.frontend.vue.template/boilerplate/src/apps/demo-component/components/CustomComp.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+	<p>myprop1_oldVal : {{myprop1_oldVal}}</p>
+	<p>myprop1_newVal : {{myprop1_newVal}}</p>
+	<p>myprop1 : {{myprop1}}</p>
+</div>
+</template>
+
+<script>
+export default {
+
+	props : [
+		'myprop1'
+	],
+
+	data : ()=>({
+		myprop1_oldVal : null,
+		myprop1_newVal : null,
+	}),
+
+	watch : {
+		myprop1(newVal, oldVal) { // watch it
+			this.myprop1_oldVal = oldVal;
+			this.myprop1_newVal = newVal;
+          console.log('Prop changed: ', newVal, ' | was: ', oldVal)
+        }
+	}
+
+}
+</script>
+
+<style>
+
+</style>
