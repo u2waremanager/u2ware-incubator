37589dc880aad9b372e3a2fc868fe8a125b439f9
u2waremanager@gmail.com <u2waremanager@gmail.com>
2021-02-17 17:03:52 +0900

    -

---
+++ io.github.u2ware.spring.oauth2.login.client/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import './plugins/axios'
+import './plugins/moment'
+import main from './main.vue'
+import vuetify from './plugins/vuetify';
+import i18n from './plugins/i18n'
+import router from './plugins/router'
+
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  i18n,
+  router,
+  render: h => h(main)
+}).$mount('#app')
